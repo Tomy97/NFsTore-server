64cbf4ab4b90d320852e9ed41f4357408ad35194
Tomas Pandullo <tomas_pandullo@div-it.com.ar>
2022-10-31 22:51:33 -0300

    mas cambios

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 
 module.exports = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'nftstore',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: 'mysql',
---
+++ src/env.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv'
+import fs from 'fs'
+import path from 'path'
+
+const defaults: Record<string, any> = {
+  CONFIG_PATH: path.resolve(process.cwd()),
+}
+
+let env: Record<string, any> = {
+  ...defaults,
+  ...process.env,
+  ...getEnv(),
+}
+process.env = env
+export default env
+
+function getEnv() {
+  const configPath = path.resolve(
+    process.env.PWD || defaults.CONFIG_PATH,
+    '.env',
+  )
+  if (fs.existsSync(configPath) === false) return {}
+  return dotenv.parse(fs.readFileSync(configPath, { encoding: 'utf8' }))
+}
