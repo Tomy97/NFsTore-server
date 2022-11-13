3d9689ee658013c822c3fbe84de3460abf1b1367
Tomas Pandullo <tomas.pandu@gmail.com>
2022-11-13 20:09:41 -0300

    .

--- src/Controllers/auth.controller.ts
+++ src/Controllers/auth.controller.ts
@@ -1,20 +1,43 @@
-import { Users } from "../Models/users.model";
-import { Response, Request } from "express";
-import jwt from "jsonwebtoken";
+import { Users } from '../Models/users.model'
+import { Response, Request } from 'express'
+import { generateJWT } from '../Helper/generate-jwt'
+import { validationResult } from "express-validator";
+
 export const authLogin = async (req: Request, res: Response) => {
-    try {
-        const {email, password} = req.body;
-        const user = await Users.findOne({where: {email}});
-        if (!user) {
-            return res.status(400).json({message: 'User not found'})
-        }
-        // const validPassword = await bcrypt.compare(password, user.password);
-        // if (!validPassword) {
-        //     return res.status(400).json({message: 'Invalid password'})
-        // }
-        // const token = jwt.sign({id: user.id}, {expiresIn: '1h'});
-        // return res.json({token})
-    } catch (err: any) {
-        return res.status(500).json({message: err.message})
+  const { email, password } = req.body
+  try {
+    const usuario = await Users.findOne()
+    if (!email) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El email es obligatorio',
+      })
     }
+    if (!password) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'La contraseña es obligatoria',
+        })
+    }
+
+    const token = await generateJWT(usuario.id)
+    res.json({
+        usuario,
+        token
+    })
+  } catch (err) {
+    return res.status(500).json({ message: err })
+  }
+}
+
+export const registerUser = async ( req: Request, res: Response ) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            ok: false,
+            errors: errors.array()
+        });
+    }
+
+
 }
--- src/Controllers/user.controller.ts
+++ src/Controllers/user.controller.ts
@@ -1,36 +1,30 @@
-// import {Users} from "../Models/users.model";
 import { Users } from '../Models/users'
-import {Response} from "express";
+import { Response } from 'express'
 
 export const getUsersController = async (req: any, res: Response) => {
-    try {
-        const users =
-            await Users.findAll(
-                {
-                    attributes: req.attributes,
-                }
-            );
-        res.json(users)
-    } catch (err: any) {
-        res.status(500).json({message: err.message})
-    }
+  try {
+    const users = await Users.findAll({
+      attributes: req.attributes,
+    })
+    res.json(users)
+  } catch (err) {
+    res.status(500).json({ message: err })
+  }
 }
 
 export const postUsersController = async (req: any, res: Response) => {
-    try {
-        const user = await Users.create(req.body)
-        if ( user.getDataValue('email') === req.body.email) {
-            console.log('User created successfully')
-        }
-        else if (user.getDataValue('email') !== req.body.email) {
-            console.log('User not created')
-        }
-        else if (user.getDataValue('email') === null) {
-            console.log('User not created')
-        }
-
-        res.json(user)
-    } catch (err: any) {
-        res.status(500).json({message: err.message})
+  try {
+    const user = await Users.create(req.body)
+    if (user.getDataValue('email') === req.body.email) {
+      console.log('User created successfully')
+    } else if (user.getDataValue('email') !== req.body.email) {
+      console.log('User not created')
+    } else if (user.getDataValue('email') === null) {
+      console.log('User not created')
     }
+
+    res.json(user)
+  } catch (err) {
+    res.status(500).json({ message: err })
+  }
 }
