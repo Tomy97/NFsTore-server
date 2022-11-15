62a39dd1794be3b7349f282c019aa41f6c67a4e5
Nicolas Capiz <nicolascapiz98@gmail.com>
2022-11-14 22:47:25 -0300

    update

---
+++ src/Models/collection.model.ts
@@ -0,0 +1,30 @@
+
+import {DataTypes} from "sequelize";
+import sequelize from "../Conections";
+
+export const Collection = sequelize.define('collection', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    type: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    nft_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true
+    },
+
+});
